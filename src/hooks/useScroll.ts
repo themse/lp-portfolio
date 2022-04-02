7fa7ef0ae0fdc01a75d007947477e64e17bdef4b
Den Polusmyak <den.polusmyak@gmail.com>
2022-04-03 00:13:53 +0300

    fixed sticky navbar when scroll

--- src/hooks/useScroll.ts
+++ src/hooks/useScroll.ts
@@ -8,13 +8,16 @@ export const useScroll = (): void => {
   useEffect(() => {
     //   HACK
     setTimeout(() => {
+      const navbarEl = document.getElementById('navbar');
+      const navbarHeight = navbarEl?.getBoundingClientRect().height ?? 0;
+
       const elId = history.location.hash.replace('#', '');
       const element = document.getElementById(elId);
       const topPosition = element?.getBoundingClientRect().top ?? 0;
 
       if (topPosition > 10) {
         window.scrollTo({
-          top: topPosition,
+          top: topPosition - navbarHeight,
         });
       }
     }, 500);
